80e1a6de24cdb4f614d3cef43049a876f6210971
Dasari <sree91@gmail.com>
2023-08-25 23:43:49 +0530

    Enumerate list
    Here we use the enumerate method to loop over a list. We unpack a tuple of (index, element) at each iteration of the loop.

---
+++ Lists/list9.py
@@ -0,0 +1,7 @@
+colors = ["green", "orange", "yellow"]
+
+# Call enumerate() to access indexes and elements.
+# ... Unpack each tuple returned from enumerate().
+for i, color in enumerate(colors):
+    print("INDEX:", i)
+    print("COLOR:", color)
